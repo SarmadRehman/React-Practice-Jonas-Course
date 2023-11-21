3beeffcc442ea484a80745eeb6b1f4873554d385
Sarmad Rehman <sarmadr02@gmail.com>
2023-11-22 03:33:47 +0500

    Event handling

--- steps/src/App.js
+++ steps/src/App.js
@@ -6,6 +6,14 @@ const messages = [
 
 function App() {
   const step = 1;
+  function handlePrevious() {
+    alert("Previous");
+  }
+
+  function handleNext() {
+    alert("Next");
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -17,10 +25,18 @@ function App() {
         Step {}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ background: "#7920f2", color: "#fff" }}>
+        <button
+          style={{ background: "#7920f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ background: "#7920f2", color: "#fff" }}>Next</button>
+        <button
+          style={{ background: "#7920f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
